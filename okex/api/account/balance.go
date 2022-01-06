a48e2d0be2533a6c0ae7a6665e496f915da7a08b
aping <czp010443@aliyun.com>
2022-01-06 22:23:14 +0800

    string -> float

--- okex/api/account/balance.go
+++ okex/api/account/balance.go
@@ -36,33 +36,33 @@ type Balance struct {
 	Mmr         string           `json:"mmr,omitempty"`
 	NotionalUsd string           `json:"notionalUsd,omitempty"`
 	OrdFroz     string           `json:"ordFroz,omitempty"`
-	TotalEq     string           `json:"totalEq"`
-	UTime       string           `json:"uTime"`
+	TotalEq     float64          `json:"totalEq,string"`
+	UTime       uint             `json:"uTime,string"`
 	Details     []*BalanceDetail `json:"details"`
 }
 
 type BalanceDetail struct {
-	AvailBal      string `json:"availBal,omitempty"`
-	AvailEq       string `json:"availEq,omitempty"`
-	CashBal       string `json:"cashBal"`
-	Ccy           string `json:"ccy"`
-	CrossLiab     string `json:"crossLiab,omitempty"`
-	DisEq         string `json:"disEq"`
-	Eq            string `json:"eq"`
-	EqUsd         string `json:"eqUsd"`
-	FrozenBal     string `json:"FrozenBal"`
-	Interest      string `json:"interest,omitempty"`
-	IsoEq         string `json:"isoEq,omitempty"`
-	IsoLiab       string `json:"isoLiab,omitempty"`
-	IsoUpl        string `json:"isoUpl,omitempty"`
-	Liab          string `json:"liab,omitempty"`
-	MaxLoan       string `json:"maxLoan,omitempty"`
-	MgnRatio      string `json:"mgnRatio,omitempty"`
-	NotionalLever string `json:"notionalLever,omitempty"`
-	OrdFrozen     string `json:"ordFrozen"`
-	StgyEq        string `json:"stgyEq,omitempty"`
-	Twap          string `json:"twap,omitempty"`
-	UTime         string `json:"uTime"`
-	Upl           string `json:"upl,omitempty"`
-	UplLiab       string `json:"uplLiab,omitempty"`
+	AvailBal      string  `json:"availBal,omitempty"`
+	AvailEq       string  `json:"availEq,omitempty"`
+	CashBal       float64 `json:"cashBal,string"`
+	Ccy           string  `json:"ccy"`
+	CrossLiab     string  `json:"crossLiab,omitempty"`
+	DisEq         float64 `json:"disEq,string"`
+	Eq            float64 `json:"eq,string"`
+	EqUsd         float32 `json:"eqUsd,string"`
+	FrozenBal     float64 `json:"FrozenBal,string"`
+	Interest      string  `json:"interest,omitempty"`
+	IsoEq         string  `json:"isoEq,omitempty"`
+	IsoLiab       string  `json:"isoLiab,omitempty"`
+	IsoUpl        string  `json:"isoUpl,omitempty"`
+	Liab          string  `json:"liab,omitempty"`
+	MaxLoan       string  `json:"maxLoan,omitempty"`
+	MgnRatio      string  `json:"mgnRatio,omitempty"`
+	NotionalLever string  `json:"notionalLever,omitempty"`
+	OrdFrozen     float64 `json:"ordFrozen,string"`
+	StgyEq        string  `json:"stgyEq,omitempty"`
+	Twap          string  `json:"twap,omitempty"`
+	UTime         uint    `json:"uTime,string"`
+	Upl           string  `json:"upl,omitempty"`
+	UplLiab       string  `json:"uplLiab,omitempty"`
 }
